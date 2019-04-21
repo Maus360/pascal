01ee27fe92612a7144ea0d6246b1b1de1ddf49be
Maks <maksimochka200@gmail.com>
2019-04-21 20:42:21 +0300

    add sign up link to web-pages

--- aipos/pascal/views.py
+++ aipos/pascal/views.py
@@ -19,7 +19,7 @@ def index(request):
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("index")
     template_name = "registration/signup.html"
 
 
