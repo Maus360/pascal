3d4b8497f9303c40678a85600979385c8aa4d07d
Maks <maksimochka200@gmail.com>
2019-04-21 22:03:00 +0300

    Auto stash before revert of "fix settings + change index.html"

--- aipos/pascal/views.py
+++ aipos/pascal/views.py
@@ -19,7 +19,7 @@ def index(request):
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("index")
+    success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
 
