329c0d0099fc8d456f8111a443a86e25007f7e25
Maks <maksimochka200@gmail.com>
2019-04-27 17:58:25 +0300

    debug =true

--- aipos/settings.py
+++ aipos/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "pgdn!$uwt3a=h5t+w7d!fnkf^*)nncbuv(1w-+(76%v1a!81$-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com"]
 
