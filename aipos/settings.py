bf919e9b2b520dbbb4269203dbcd8f9f68501d32
Maks <maksimochka200@gmail.com>
2019-04-27 19:13:04 +0300

    login via github google

--- aipos/settings.py
+++ aipos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "aipos.pascal",
+    "social_django",
 ]
 
 MIDDLEWARE = [
@@ -63,10 +64,21 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "social_django.context_processors.backends",  # <- Here
+                "social_django.context_processors.login_redirect",
             ]
         },
     }
 ]
+
+AUTHENTICATION_BACKENDS = (
+    "social_core.backends.open_id.OpenIdAuth",  # for Google authentication
+    "social_core.backends.google.GoogleOpenId",  # for Google authentication
+    "social_core.backends.google.GoogleOAuth2",  # for Google authentication
+    "social_core.backends.github.GithubOAuth2",  # for Github authentication
+    "social_core.backends.facebook.FacebookOAuth2",  # for Facebook authentication
+    "django.contrib.auth.backends.ModelBackend",
+)
 
 WSGI_APPLICATION = "aipos.wsgi.application"
 
@@ -135,7 +147,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = (
+    "410288448737-qt3ubkkc4ihak13i78c03aeh1o2a82ca.apps.googleusercontent.com"
+)  # Paste CLient Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "1O-ByLcEJSK5NJccRq6ReHSs"
 
+SOCIAL_AUTH_GITHUB_KEY = "07ae1132f5d559f8d750"  # Paste Client ID
+SOCIAL_AUTH_GITHUB_SECRET = (
+    "40a7f06a27de0a4cc36889c621d5a415f3a48198"
+)  # Paste Secret Key  # Paste Secret Key
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
