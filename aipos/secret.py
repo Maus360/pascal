ce074067ac348cb3333456c1a9f4e1e6303e20f0
Maks <maksimochka200@gmail.com>
2019-05-03 20:06:36 +0300

    secret

---
+++ aipos/secret.py
@@ -0,0 +1,12 @@
+SECRET_KEY = "pgdn!$uwt3a=h5t+w7d!fnkf^*)nncbuv(1w-+(76%v1a!81$-"
+LOGIN_REDIRECT_URL = 'index'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = (
+    "410288448737-qt3ubkkc4ihak13i78c03aeh1o2a82ca.apps.googleusercontent.com"
+)  # Paste CLient Key
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "1O-ByLcEJSK5NJccRq6ReHSs"
+
+SOCIAL_AUTH_GITHUB_KEY = "07ae1132f5d559f8d750"  # Paste Client ID
+SOCIAL_AUTH_GITHUB_SECRET = (
+    "40a7f06a27de0a4cc36889c621d5a415f3a48198"
+)  # Paste Secret Key  # Paste Secret Key
